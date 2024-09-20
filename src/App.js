9c6c30f7bf415be1e03b3e172c6a290150d30471
Rahulworldcoder2975 <rahulmalhotradream@gmail.com>
2024-09-20 18:39:06 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from "./Pages/Home"
+import Profile from "./Pages/Profile";
+const  App = () => {
+  return (
+    <div className="App">
+      <Home/>
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
